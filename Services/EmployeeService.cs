84ea6da1048730fdbb43406f7e06c5e46c826032
satyendra <satyendra@satya2021>
2021-07-28 14:58:57 +0000

    Add project files.

---
+++ Services/EmployeeService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiEmp.IServices;
+using WebApiEmp.Models;
+
+namespace WebApiEmp.Services
+{
+	public class EmployeeService: IEmployeeService
+	{
+
+		elifedealContext dbContext;
+		public EmployeeService(elifedealContext _db)
+		{
+			dbContext = _db;
+		}
+
+        public IEnumerable<Employee> GetEmployee()
+        {
+            var employee = dbContext.Employees.ToList();
+            return employee;
+        }
+        public Employee AddEmployee(Employee employee)
+        {
+            if (employee != null)
+            {
+                dbContext.Employees.Add(employee);
+                dbContext.SaveChanges();
+                return employee;
+            }
+            return null;
+        }
+        public Employee UpdateEmployee(Employee employee)
+        {
+            dbContext.Entry(employee).State = EntityState.Modified;
+            dbContext.SaveChanges();
+            return employee;
+        }
+        public Employee DeleteEmployee(int id)
+        {
+            var employee = dbContext.Employees.FirstOrDefault(x => x.Id == id);
+            dbContext.Entry(employee).State = EntityState.Deleted;
+            dbContext.SaveChanges();
+            return employee;
+        }
+        public Employee GetEmployeeById(int id)
+        {
+            var employee = dbContext.Employees.FirstOrDefault(x => x.Id == id);
+            return employee;
+        }
+    }
+}
